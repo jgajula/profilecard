23b2398dc8d2aa848895f1b930b49290c8f489ff
Jagadeesh Gajula <jagadish.d.one@gmail.com>
2012-05-06 16:04:54 -0700

    first files
    asdf

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="/css/styles.css" rel="stylesheet" type="text/css">
+    </head>
+    
+<body style="margin: 0px;font: 12px/18px 'Lucida Grande', 'Lucida Sans Unicode', Helvetica, Arial, Verdana, sans-serif;
+font-family: 'Lucida Grande', 'Lucida Sans Unicode', Helvetica, Arial, Verdana, sans-serif;
+font-size: 12px;
+font-style: normal;
+font-variant: normal;
+font-weight: normal;
+line-height: 18px;">
+    <div id="bodyWrapper">
+        <div id="headerWrapper">
+            <div id="header" style="width:100%;background-color: #fdb82b;height:80px;">
+                <div id="headerContentWrapper" style="width:900px;margin: auto;">
+                    <div id="logoWrapper" style="width: 94px;float: left;padding-top: 20px;">
+                        <img src="/images/logo.png"/>
+                    </div>
+                    <div id="demoLinkWrapper" style="width: 200px;margin: auto;padding-top: 30px;">
+                        <span>HOW DOES IT WORK?</span>
+                    </div>
+                    <div id="accountRibbon" style="float: right;margin-top: -20px;">
+                        <span>REGISTER | LOGIN | HELP</span>
+                    </div>    
+                </div>
+            </div>
+        </div>
+        <div id="contentWrapper">
+            <div id="contentBodyWrapper" style="width: 900px;margin: auto;">
+                <div id="contentBody" style="padding-left: 100px;">
+                    <div id="getYoursDiv" style="margin-top: 100px;margin-bottom: 20px;">
+                        <div id="getYoursTextDiv" style="float: left;padding-right: 20px;">
+                            <span>GET YOURS USING</span>
+                        </div>
+                        <div id="facebookLogoWrapper" style="float: left;margin-top: -5px;padding-right: 20px;">
+                            <img src="/images/facebook_logo.png"/>
+                        </div>
+                        <div id="twitterLogoWrapper" style="float: left;margin-top: -5px;padding-right: 20px;">
+                            <img src="/images/twitter_logo.png"/>
+                        </div>
+                        <div id="onTheFlyWrapper">
+                            <span>ON THE FLY</span>
+                        </div>
+                    </div>
+                    <div id="imageGalleryWrapper">
+                        <div id="imageHolderWrapper" style="float: left;padding-right: 10px;">
+                            <img id="tajCardImage" src="/images/tajCard.png" width="600px;" style="box-shadow: 3px 3px 4px #000;"/>
+                            <img id="effielCardImage" src="/images/effielCard.png" width="600px;" style="display:none;box-shadow: 3px 3px 4px #000;"/>
+                        </div>
+                        
+                        <div id="imageAlbumWrapper">
+                            <div id="albumEntryOne" onmouseover="document.getElementById('effielCardImage').style.display='none';document.getElementById('tajCardImage').style.display='block';">
+                                <img src="/images/effielTower.png" class="albumEntry"/>
+                            </div>
+                             <div id="albumEntryTwo" onmouseover="document.getElementById('tajCardImage').style.display='none';document.getElementById('effielCardImage').style.display='block';">
+                                <img src="/images/londonTower.png" class="albumEntry"/>
+                             </div>
+                             <div id="albumEntryThree" onmouseover="document.getElementById('effielCardImage').style.display='none';document.getElementById('tajCardImage').style.display='block';">
+                                <img src="/images/effielTower.png" class="albumEntry"/>
+                            </div>
+                             <div id="albumEntryFour" onmouseover="document.getElementById('tajCardImage').style.display='none';document.getElementById('effielCardImage').style.display='block';">
+                                <img src="/images/londonTower.png" class="albumEntry"/>
+                            </div>
+                            <div id="albumEntryFive" onmouseover="document.getElementById('effielCardImage').style.display='none';document.getElementById('tajCardImage').style.display='block';">
+                                <img src="/images/effielTower.png" class="albumEntry"/>
+                            </div>
+                            <div id="albumEntryFour" onmouseover="document.getElementById('tajCardImage').style.display='none';document.getElementById('effielCardImage').style.display='block';">
+                                <img src="/images/londonTower.png" class="albumEntry" />
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+                
+            </div>
+        </div>
+        <div id="footerWrapper" style="clear: both;margin-top: 100px;width: 100%;background-color: #999;height: 80px;">
+            <div id="footer" style="width: 900px;margin: auto;">
+                <div id="footerLinks" style="padding-top: 30px;text-align: center;">
+                <span>YOUR ACCOUNT | ABOUT US | SUPPORT | BLOG | PRIVACY | TERMS</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
